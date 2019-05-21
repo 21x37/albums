0ee8bf00ed3ab6de2bccff544e38dcb052c6e3d6
21x37 <tylermok@outlook.com>
2019-05-20 21:36:16 -0700

    started fetching data from api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            albums: []
+        }
     }
-    componentWillMount() {
-        console.log('mounted')
-
+    async componentWillMount() {
+        const { data } = await axios.get('https://rallycoding.herokuapp.com/api/music_albums');
+        await this.setState({ albums: data })
+    };
+    renderAlbums() {
+        return this.state.albums.map((album) => {
+            console.log(album.title);
+            return <Text>{album.title}</Text>
+        })
     }
     render() {
         return (
             <View>
-                <Text>Album list!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
