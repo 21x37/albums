af4fc86350a6576bfc7bb690ffc5a37369292e2c
21x37 <tylermok@outlook.com>
2019-05-22 19:10:15 -0700

    styled album cards

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = (props) => {
+    console.log(props.album.thumbnail_image)
+    return (
+        <Card>
+            <CardSection>
+                <View style={styles.thumbnailContainer}>
+                    <Image style={styles.thumbnailStyle} source={{ uri: props.album.thumbnail_image }}/>
+                </View>
+                <View style={styles.contentStyle}>
+                    <Text style={styles.headerText}>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image style={styles.imageStyle} source={{ uri: props.album.image }}></Image>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    contentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerText: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+}
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends React.Component {
     constructor(props) {
@@ -15,10 +16,9 @@ class AlbumList extends React.Component {
     };
     renderAlbums() {
         return this.state.albums.map((album) => {
-            console.log(album.title);
-            return <Text>{album.title}</Text>
-        })
-    }
+            return <AlbumDetail key={album.title} album={album}></AlbumDetail>
+        });
+    };
     render() {
         return (
             <View>
